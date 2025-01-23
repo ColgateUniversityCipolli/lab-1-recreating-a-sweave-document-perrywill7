8076d34fa01504b84f7622028d1520a5ec76291d
perrywill7 <wperry@colgate.edu>
2025-01-23 16:02:24 -0500

    L1 2

---
+++ L1Tester.R
@@ -0,0 +1,14 @@
+\begin{figure}[H]
+\begin{center}
+%This code is evaluated, but not printed
+%warning=FALSE and message=FALSE ensure no text is returned as a by product of
+%printing; this would cause an error in that plot+text is returned instead of
+%just the plot
+<<echo=FALSE, warning=FALSE, message=FALSE, fig.dim=c(5,3.5)>>=
+  <<plot1>>
+  @
+  \caption{A histogram of random normally distributed data, $n=100$.}
+\label{plot1} %we can now reference plot1
+\end{center}
+\end{figure}
+
